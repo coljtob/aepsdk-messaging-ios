c7a18e3ba8be2d37803710981b5be491266d5a3a
coljtob <ctobin@adobe.com>
2024-01-13 09:00:13 -0500

    saving place

--- TestApps/MessagingDemoApp/ViewController.swift
+++ TestApps/MessagingDemoApp/ViewController.swift
@@ -37,6 +37,8 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        // va7: 3149c49c3910/84e9aaeba325/launch-f58d479d8188-development
+        MobileCore.configureWith(appId: "3149c49c3910/84e9aaeba325/launch-f58d479d8188-development")
         MobileCore.messagingDelegate = messageHandler
         field.delegate = self
     }
